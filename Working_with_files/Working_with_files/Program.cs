6af5ffa1ae4c451186a93d4113776aa0140747cb
satsargsyan <satenik.sargsyan2017@gmail.com>
2017-03-05 00:35:59 +0400

    The 1-st example

---
+++ Working_with_files/Working_with_files/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Working_with_files
+{
+
+    class Test
+    {
+        public static void Main()
+        {
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader("TestFile.txt"))
+                {
+                    string line;
+                    // Read and display lines from the file until the end of 
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+                Console.ReadKey(); 
+            }
+        }
+    }
+}
